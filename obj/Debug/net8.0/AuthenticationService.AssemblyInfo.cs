cc99163de4698f40bb4c2c27243dbda44dd35f5b
shahidkm <kmshahid432@gmail.com>
2025-03-20 13:34:19 +0530

    port updated

--- obj/Debug/net8.0/AuthenticationService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthenticationService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6183bc2b9c831238d7dd0e211cbfe48193d0883")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
